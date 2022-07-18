91faa63225d16ef28166bc2159b4085685a3479d
Denisa Badarau <teodora.acatrinei@5psolutions.eu>
2022-07-18 12:33:13 +0300

    #12 Index http://pm.5psolutions.net/public/index.php?path_info=projects/training-teodora-acatrinei/tasks/12 added the laravel project with the Index page

---
+++ resources/views/products.blade.php
@@ -0,0 +1,28 @@
+<x-layout>
+    <table class="table text-light">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Price</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($products as $product)
+            <tr>
+                <th scope="row">{{ $product->id }}</th>
+                <td>{{ $product->title }}</td>
+                <td>{{ $product->description }}</td>
+                <td>{{ $product->price }}</td>
+                <td><img src="{{ asset("img/$product->img") }}" alt="album image"></td>
+                <td>
+                    <button type="button" class="btn btn-primary">Add</button>
+                </td>
+            </tr>
+        </tbody>
+    @endforeach
+</x-layout>
+
